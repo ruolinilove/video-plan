634cb9d04b1c3956f9315f328dcb63144522b3de
ruolinilove <ruolinilove@gmail.com>
2026-07-08 11:12:35 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,15 @@
 
+// ===============================
+// 创艺品牌数据中心配置文件
+// ===============================
+
+
+// Supabase 配置
+
+const SUPABASE_URL = 
+'https://oiiejtymrfzqdmkkhzcc.supabase.co';
+
+
+const SUPABASE_KEY = 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJvaWlpanR5bXJmenFkbWtraHpjYyIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzgzNDYxNTY5LCJleHAiOjIwOTkwMzc1NjlfLmxEQ0EtOGFhMjZjdktQbVROcTRGWEdfczF0MFZLZW5iRmJ6MHBVUXE';
+
